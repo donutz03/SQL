5a3318693e4f946d3d550442f00929571f56ff38
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2023-07-19 14:09:49 +0300

    k

--- tabeleIntroductorii.sql
+++ tabeleIntroductorii.sql
@@ -4,3 +4,17 @@ create table DEPARTMENTS (
   location varchar2(50), 
   constraint pk_departments primary key (deptno)
 );
+
+create table EMPLOYEES (
+  empno       number,
+  name        varchar2(50) not null,
+  job         varchar2(50),
+  manager     number,
+  hiredate    date,
+  salary      number(7,2),
+  comission   number(7,2),
+  deptno      number,
+  constraint pk_employees primary key (empno),
+  constraint fk_employees_deptno foreign key (deptno)
+      references DEPARTMENTS (deptno)
+  );
