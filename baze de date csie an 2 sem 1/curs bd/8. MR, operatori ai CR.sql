d50c836128e906eaa91efa7c0832e486bfbc2bfe
donutz03 <109514731+donutz03@users.noreply.github.com>
2023-09-07 23:05:41 +0300

    structure of select

--- baze de date csie an 2 sem 1/curs bd/8. MR, operatori ai CR.sql
+++ baze de date csie an 2 sem 1/curs bd/8. MR, operatori ai CR.sql
@@ -1,1 +1,10 @@
+comanda select
 
+SELECT ALL | DISTINCT {nume_tabela* | expr[sinonim], expr[sinonim], ...} FROM nume_tabela [@legatura][sinonim], ... nume_tabela [@legatura][sinonim], [WHERE conditie]
+[CONNECT BY conditie [START WITH conditie] ]
+[GROUP BY {expr, expr.. | CUBE(expr, expr..) | ROLLUP (expr, expr..)}]
+[HAVING conditie]
+[{UNION | INTERSECT | MINUS } SELECT ... ]
+[ORDER BY { expr | numar-pozitie} [ASC | DESC]
+{expr | numar-pozitie} [ASC | DESC ], ...
+[FOR UPDATE OF nume-col, nume-col, ... [NOWAIT] ];
